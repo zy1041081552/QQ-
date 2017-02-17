55226b468002cde6e30bd2eb759cb1ab68740d0a
qianfeng <qianfeng@aaa.local>
2017-02-17 18:30:53 +0800

    first

---
+++ qq/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  qq
+//
+//  Created by qianfeng on 17/2/17.
+//  Copyright © 2017年 qianfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ qq/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  qq
+//
+//  Created by qianfeng on 17/2/17.
+//  Copyright © 2017年 qianfeng. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
